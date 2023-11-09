5846d0b3b2db7a533a3406b6941c81574e10ac5c
Marton Szegheo <mszegheo@engineer.com>
2023-11-09 18:39:37 +0100

    pipeline init
    Change-Id: I464eefa15c96bab38749d8a2d0ed2951a3181bdd

---
+++ test/createconfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p test/config
+cp test/config.yaml test/config/config.yaml
+
+#sed -i -e "s|@hostaddress@|$1|" -e "s|@port@|$2|" -e "s|@user@|$3|" -e "s|@password@|$3|" -e "s|@dbname@|$4|" test/config/config.yaml
+sed -i -e "s|@hostaddress@|${HOST}|" -e "s|@port@|${PORT}|" -e "s|@user@|${USERNAME}|" -e "s|@password@|${PASSWORD}|" -e "s|@dbname@|${DBNAME}|" test/config/config.yaml
+
+echo "Prepared config file:"
+cat test/config/config.yaml
